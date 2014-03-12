332b5c9615dafa9bb40d2b8a1c108cd6eff61469
nxsofsys <nxsofsys@gmail.com>
2014-03-12 17:54:51 +0400

    no message

---
+++ PreviewExt/patterns_preview.py
@@ -0,0 +1,22 @@
+patterns.append({
+'name': 'TILSample',
+
+'signature': { 'item' : '',
+               'label' : '', 
+               'p2type' : '', 
+               'source' : '',
+               'date' : '',
+               'stw' : ''
+               },
+'options': [ 
+          {
+          'item' : 'self',
+          'p2type' : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+          'label' : 'label',
+          'source' : 'annSource',
+          'date' : 'annCreationDate',
+          'stw': 'annStewardshipContact',
+          },
+     ]
+     },
+)
