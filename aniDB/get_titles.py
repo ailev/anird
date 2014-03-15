98f64261abacf95501058a55a181cd43fb2d9ae4
Ivan Kurmanov <duraley@gmail.com>
2014-03-16 00:14:53 +0300

    script can now be called from another directory

--- aniDB/get_titles.py
+++ aniDB/get_titles.py
@@ -5,17 +5,19 @@ from urllib import urlretrieve
 import gzip
 import time
 
+base_dir = os.path.dirname( __file__ );
+
 # test values
 if 1:
 	TITLES_XML_GZ_URL = 'http://ahinea.com/qvwm/qvwm-1.1.12-9-IKu.tar.gz'
 	TITLES_XML_GZ_LOCALFILE = 'tmp/qvwm-1.1.12-9-IKu.tar.gz'
-	TITLES_XML_LOCALFILE = 'tmp/qvwm-1.1.12-9-IKu.tar'
+	TITLES_XML_LOCALFILE    = 'tmp/qvwm-1.1.12-9-IKu.tar'
 
 # real values
 if 1:
 	TITLES_XML_GZ_URL = 'http://anidb.net/api/anime-titles.xml.gz'
 	TITLES_XML_GZ_LOCALFILE = 'data/anime-titles.xml.gz'
-	TITLES_XML_LOCALFILE = 'data/anime-titles.xml'
+	TITLES_XML_LOCALFILE    = 'data/anime-titles.xml'
 
 
 def retrieve_hook(count,size,total):
@@ -29,28 +31,32 @@ def retrieve_hook(count,size,total):
 def main():
 	
 	url          = TITLES_XML_GZ_URL
-	filename_gz  = TITLES_XML_GZ_LOCALFILE
-	filename_xml = TITLES_XML_LOCALFILE
+	filename_gz  = os.path.join( base_dir, TITLES_XML_GZ_LOCALFILE )
+	filename_xml = os.path.join( base_dir, TITLES_XML_LOCALFILE )
 
 	retrieve = False
 	update   = False
 	
+	print "file %s:" % filename_gz
+	
 	if os.path.isfile( filename_gz ):
 		mtime = os.path.getmtime( filename_gz )
-		print "last modified: %s (ctime)" % time.ctime( mtime )
+		print "...last modified: %s (ctime)" % time.ctime( mtime )
 		if time.time() > mtime + 24*60*60:
-			print "more than a day has passed. we can re-request the file now"
+			print "...more than a day has passed. we can re-request the file now"
 			retrieve = True
 			update = True
 		else:
-			print "it is new enough, no need to download it again"
+			print "...is new enough"
 	else:
 		retrieve = True
 
 	if retrieve:
 		urlretrieve( url, filename_gz, retrieve_hook )
 	else: 
-		print "file %s is already present" % filename_gz
+		print "...no need to download it (again)"
+	
+	print "file %s:" % filename_xml
 	
 	if not os.path.isfile( filename_xml ) or update:
 		f = gzip.open( filename_gz, 'rb')
@@ -60,12 +66,9 @@ def main():
 		fout = open( filename_xml, 'wb' )
 		fout.writelines( file_content )
 		fout.close()
-		print "file %s is ready" % filename_xml
-	else: 
-		print "file %s is already present" % filename_xml
+		print "...extracted"
 		
 	if os.path.isfile( filename_xml ):
-		print "ready to parse %s" % filename_xml
-
+		print "...ready to be parsed"
 
 main()
